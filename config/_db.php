d1f58648f95cd7fd4f87ea0056acf7b7c6284b06
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2023-08-02 21:51:29 +0200

    Add missing config files

---
+++ config/_db.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return (object) [
+  "use_db" => true,
+  "host" => "DB_HOST",
+  "user" => "DB_USER",
+  "password" => "DB_PASSWORD",
+  "dbname" => "DB_NAME",
+];
---
+++ config/_email.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return (object) [
+  "use_emailer" => false,
+  "username" => "EMAIL_USERNAME",
+  "host" => "EMAIL_HOST",
+  "password" => "EMAIL_PASSWORD",
+  "replyTo" => "EMAIL_REPLYTO",
+  "replyToName" => "EMAIL_REPLYTO_NAME",
+  "fromName" => "EMAIL_FROMNAME"
+];
